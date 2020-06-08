fd0c74bae5b778e902b236c18d97e33944d0900e
samridhiprasad19 <samridhiprasad19@gmail.com>
2020-06-08 20:52:19 +0530

    url_for,if..else usage

--- application/routes.py
+++ application/routes.py
@@ -3,4 +3,14 @@ from flask import render_template
 @app.route("/")
 @app.route("/index")
 def index():
-   return render_template('index.html')
+   return render_template("index.html",login = True)
+@app.route("/classes")
+def classes():
+   return render_template("classes.html",login = True)
+@app.route("/register")
+def register():
+   return render_template("register.html",login = True)
+@app.route("/login")
+def login():
+   return render_template("login.html",login = True)
+
